3c63a14b3c7f1969a86e690f967ca0299e853987
Anthonin <Anthonincolas37000@gmail.com>
2022-08-31 13:30:10 +0200

    gallery

--- src/Form/GalleryType.php
+++ src/Form/GalleryType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Gallery;
-use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
